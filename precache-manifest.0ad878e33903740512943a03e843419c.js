0fa7c0171711600315199647474887dd9eeb8e19
przekwas <luke@covalence.io>
2019-03-15 12:38:35 -0500

    Updates

---
+++ precache-manifest.0ad878e33903740512943a03e843419c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fde3c389f81993c1a6ca",
+    "url": "/static/js/main.60e26a9c.chunk.js"
+  },
+  {
+    "revision": "302c921ee172abcdf4dd",
+    "url": "/static/js/2.067d0358.chunk.js"
+  },
+  {
+    "revision": "302c921ee172abcdf4dd",
+    "url": "/static/css/2.059189f1.chunk.css"
+  },
+  {
+    "revision": "df71be5174917ea9ceacfde3b9a5d137",
+    "url": "/index.html"
+  }
+];
